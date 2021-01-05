ac8eac48bf4ce0e80aa046b453d66bf470bbf545
ChristianGerlin <christiang@disroot.org>
2021-01-05 03:45:32 +0100

    neues design sublinks 1

---
+++ src/nav/navO.js
@@ -0,0 +1,15 @@
+$("#nav-placeholder-O").load("/src/nav/navO.html");
+var current = location.pathname.split("/")[1];
+
+if(current == "m.kontakt.html"){
+    current = "kontakt.html";
+}
+
+if(current == "m.twitter.timeline1.html" || current == "m.twitter.timeline2.html" || current == "m.twitter.timeline3.html"){
+    current = "twitter.html";
+}
+
+function isActive(){
+    console.log(current);
+    document.getElementById(current).classList.add('active');
+}
